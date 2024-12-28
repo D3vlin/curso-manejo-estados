c82b2dbcaa76dc6f80e2b02f53a98eee71d9419a
D3vlin <alexisdq16@gmail.com>
2024-12-28 12:20:18 -0500

    Declatative state with React.useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { UseState } from "./Components/UseState/UseState";
-import { ClassState } from "./Components/ClassState/ClassState";
+import { UseReducer } from './UseReducer';
 
 function App() {
   return (
     <div className="App">
       <UseState name="UseState" />
-      <ClassState name="ClassState" />
+      <UseReducer name="UseReducer" />
     </div>
   );
 }
--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,3 +1,7 @@
+import React from "react";
+
+const SECURITY_CODE = 'paradigma'
+
 const initialState = {
     error: false,
     loading: false,
@@ -6,52 +10,25 @@ const initialState = {
     confirmed: false
 }
 
-// const reducer = (state, action) => {
-
-// }
-
-const reducerIf = (state, action) => {
-    if (action.type === 'ERROR') {
-        return {
-            ...state,
-            error: true,
-            loading: false
-        }
-    } else if (action.type === 'CHECK') {
-        return {
-            ...state,
-            loading: true
-        }
+const reducer = (state, action) => {
+    if (reducerObject(state)[action.type]) {
+        return reducerObject(state, action.payload)[action.type]
     } else {
-        return {
-            ...initialState
-        }
+        return state
     }
 }
 
-const reducerSwitch = (state, action) => {
-    switch (action.type) {
-        case 'ERROR':
-            return {
-                ...state,
-                error: true,
-                loading: false
-            }
-
-        case 'CHECK':
-            return {
-                ...state,
-                loading: true
-            }
-
-        default:
-            return {
-                ...initialState
-            }
-    }
-}
-
-const reducerObject = (state) => ({
+const reducerObject = (state, payload) => ({
+    'CONFIRM': {
+        ...state,
+        error: false,
+        loading: false,
+        confirmed: true
+    },
+    'WRITE': {
+        ...state,
+        value: payload
+    },
     'ERROR': {
         ...state,
         error: true,
@@ -60,13 +37,75 @@ const reducerObject = (state) => ({
     'CHECK': {
         ...state,
         loading: true
+    },
+    'DELETE': {
+        ...state,
+        deleted: true
+    },
+    'RESET': {
+        ...state,
+        deleted: false,
+        confirmed: false,
+        value: ''
     }
 })
 
-const reducer = (state, action) => {
-    if (reducerObject(state)[action.type]) {
-        return reducerObject(state)[action.type]
+function UseReducer({ name }) {
+    const [state, dispatch] = React.useReducer(reducer, initialState)
+
+    React.useEffect(() => {
+        if (state.loading) {
+            setTimeout(() => {
+                if (state.value === SECURITY_CODE) {
+                    dispatch({ type: 'CONFIRM' })
+                } else {
+                    dispatch({ type: 'ERROR' })
+                }
+            }, 3000)
+        }
+    }, [state.loading])
+
+    if (!state.deleted && !state.confirmed) {
+        return (
+            <div>
+                <h2>Eliminar {name}</h2>
+                <p>Escribe el código de seguridad</p>
+                {(state.error && !state.loading) && (
+                    <p>Error: Código Incorrecto</p>
+                )}
+                {state.loading && (
+                    <p>Cargando...</p>
+                )}
+                <input
+                    placeholder="Código de seguridad"
+                    value={state.value}
+                    onChange={(event) => dispatch({ type: 'WRITE', payload: event.target.value })}
+                />
+                <button onClick={() => dispatch({ type: 'CHECK' })} >Comprobar</button>
+            </div>
+        )
+    } else if (!state.deleted && state.confirmed) {
+        return (
+            <React.Fragment>
+                <p>Confirmar acción</p>
+                <button onClick={() => dispatch({ type: 'DELETE' })}>
+                    Si
+                </button>
+                <button onClick={() => dispatch({ type: 'RESET' })}>
+                    No
+                </button>
+            </React.Fragment>
+        )
     } else {
-        return state
+        return (
+            <React.Fragment>
+                <p>Eliminado con éxito</p>
+                <button onClick={() => dispatch({ type: 'RESET' })}>
+                    Volver atrás
+                </button>
+            </React.Fragment>
+        )
     }
 }
+
+export { UseReducer }
